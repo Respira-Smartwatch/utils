edcdd5d22a25aad2ebbca8795ece158e93bbcd82
Matteo Vidali <mvidali@iu.edu>
2023-02-28 10:58:46 -0500

    Updated plotting to allow for adjustable data amounts

--- py-Chart/pychart.py
+++ py-Chart/pychart.py
@@ -5,6 +5,8 @@ from random import randint
 import serial
 import sys  # We need sys so that we can pass argv to QApplication
 import os
+import json
+
 
 S_BTN_WID = 75 # Setting up a global parameter for button width
 
@@ -96,7 +98,7 @@ class PyChart(QtWidgets.QMainWindow):
         self.x = None
         self.y = None
         self.y2 = None
-        self.setup_graph_data()
+        
 
         # pen is the line creation method: decides plot colors
         self.pens = []
@@ -114,8 +116,15 @@ class PyChart(QtWidgets.QMainWindow):
         # PLOTTING - TODO: Put this in its own method
 
         # For plot Streaming need to assign a dataline so we can update it 
-        self.data_line = self.graphWidgets[0].plot(self.x,self.y, pen=self.red_pen)
-        self.data_line2 = self.graphWidgets[0].plot(self.x, self.y2, pen=self.blue_pen)
+        self.live_lines = []
+        self.y_list = []
+        self.setup_graph_data()
+        for idx, y in enumerate(self.y_list):
+            self.live_lines.append(self.graphWidgets[0].plot(self.x, y, pen=self.pens[idx]))
+
+            #self.data_line = self.graphWidgets[0].plot(self.x,self.y, pen=self.red_pen)
+            #self.data_line2 = self.graphWidgets[0].plot(self.x, self.y2, pen=self.blue_pen)
+
 
         # Start a timer function - Need a timer to update graph at certain speed.
         self.timer = QtCore.QTimer()                        # Initialize a timer
@@ -123,6 +132,8 @@ class PyChart(QtWidgets.QMainWindow):
         self.timer.setInterval(self.sample_T_ms)  # Set the timer interval
         if self.bus:
             self.timer.timeout.connect(self.live_plot_update)   # Set timeout behaviour
+        else:
+            self.timer.timeout.connect(self.random_liveplot_update) # For testing
         self.timer.start()                                  # Start timer
 
 
@@ -173,7 +184,7 @@ class PyChart(QtWidgets.QMainWindow):
     
 
     # TODO: Store file somewhere 
-    # TODO: Read file here or at other location
+    # Done: Read file here or at other location
     # TODO: Setup File Protection!
     def file_button_pressed(self, g_id):
         """Occurs when the File button is pressed, opens a file dialogue, and gets
@@ -189,8 +200,12 @@ class PyChart(QtWidgets.QMainWindow):
             fnames = [x.split('/')[-1] for x in filenames]
             lbl_str = ", ".join(filenames)
             print(fnames)
-            self.rename_graph(self.graphWidgets[g_id], f"Graph {g_id+1} - {fnames[0]}") # TODO: FIX THIS (only uses first name)
-            self.plot_static(g_id, filenames[0])
+            
+            self.rename_graph(self.graphWidgets[g_id], f"Graph {g_id+1} - {lbl_str}") # TODO: FIX THIS (only uses first name)
+
+            #TODO Fix Pens thing (New Static Plot Method?)
+            self.plot_static(g_id, filenames[0], isolate=True, pen=None)
+
         else:
             print("No File Selected")
             lbl_str = "No File Selected"
@@ -205,8 +220,13 @@ class PyChart(QtWidgets.QMainWindow):
         """Function occurs - simply to setup x and y data for plotting 
            This is very temporary atm"""
         self.x = list(range(1000))
-        self.y = [randint(0,100) for _ in range(1000)]
-        self.y2 = [randint(0,100) for _ in range(1000)]
+        if len(self.y_list) >= 2:
+            self.y_list[0] = [randint(0,100) for _ in range(1000)]
+            self.y_list[1] = [randint(0,100) for _ in range(1000)]
+        else:
+            self.y_list.append([randint(0,100) for _ in range(1000)])
+            self.y_list.append([randint(0,100) for _ in range(1000)])
+
     
 
     def rename_graph(self, graph, name: str, col="b", sz="20pt"):
@@ -252,16 +272,19 @@ class PyChart(QtWidgets.QMainWindow):
             print(f"Unallowable Datatype: .{ext}")
             return None
 
-    def plot_static(self, g_id:int, inFile:str, isolate=True):
+    def plot_static(self, g_id:int, inFile:str, isolate=True, pen=None):
         """ Plots static data - file formats described in read_file method"""
         # Clear given plot
         if isolate:
             self.graphWidgets[g_id].clear()
         # Pull Data from file (supported files?)
         x, data = self.read_file(inFile)
+
         # Plot the data
+
         for idx, source in enumerate(data):
-            self.graphWidgets[g_id].plot(x, source, pen=self.pens[idx])
+            pen_idx = idx%len(self.pens)
+            self.graphWidgets[g_id].plot(x, source, pen=self.pens[pen_idx])
 
     def connect_serial_cli(self):
         print("No serial bus present\n\nWould you like to connect to a bus?")
@@ -301,19 +324,40 @@ class PyChart(QtWidgets.QMainWindow):
         data = str(self.bus.readline().decode()).strip().split(',')
         print(data)
 
-        data = float(data[0])
+        #data = float(data[0]) if data[0] else None
 
-        if data > 0:
+        if data>0:
             self.x = self.x[1:]
             self.x.append(self.x[-1] + 1)
-            self.y = self.y[1:]
-            self.y2 = self.y2[1:]
-            print(data)
-            data = float(data)
-            self.y.append(data)
-            self.y2.append(data2)
-            self.data_line.setData(self.x, self.y)
-            self.data_line2.setData(self.x, self.y2)
+            for idx, d in enumerate(data):
+                if idx > len(self.live_lines)-1:
+                    print(f"Max plots is {len(self.live_lines)}, Cannot plot")
+                    break
+                self.y_list[idx].pop(0)
+                new_val = float(d)
+                self.y_list[idx].append(new_val)
+                self.live_lines[idx].setData(self.x, self.y_list[idx])
+
+            #self.y = self.y[1:]
+            #self.y2 = self.y2[1:]
+            #print(data)
+            #data = float(data)
+            #self.y.append(data)
+            #self.y2.append(data2)
+            #self.data_line.setData(self.x, self.y)
+            #self.data_line2.setData(self.x, self.y2)
+        
+    def random_liveplot_update(self):
+        self.x = self.x[1:]
+        self.x.append(self.x[-1] + 1)
+
+        self.y_list[0] = self.y_list[0][1:]
+        self.y_list[1] = self.y_list[1][1:]
+        self.y_list[0].append(randint(0,100))
+        self.y_list[1].append(randint(0,100))
+
+        self.live_lines[0].setData(self.x, self.y_list[0])
+        self.live_lines[1].setData(self.x, self.y_list[1])
         
 
 

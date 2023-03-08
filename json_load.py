2192f12ecd3ece0be3341f62e80ea84a48d6297e
Matteo Vidali <mvidali@iu.edu>
2023-03-08 14:01:25 -0500

    updated json_load

--- json_load.py
+++ json_load.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
 import json
 
-data = json.load(open('respira_Max_2023-02-22.json', 'r'))
+data = json.load(open('joe.json', 'r'))
 phases = ['baseline', 'expiration1', 'rest1', 'expiration2', 'rest2', 'video', 'rest3', 'recitation', 'rest4']
 phasic = []
 tonic = []
-print(data)
+
+
 for i in phases:
-	for j in data[i]['gsr_phasic']:
-		phasic.append(j)
-for i in phases:
-	for j in data[i]['gsr_tonic']:
-		tonic.append(j)
-		
-plt.plot(phasic)
+	tonic.extend(data[i]["gsr_tonic"])	
+
+print(tonic)
+input()
+
 plt.plot(tonic)
 plt.title('GSR Test Data')
 plt.ylabel('Skin Conductance Response')
